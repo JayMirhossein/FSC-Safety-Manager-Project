f927f3613c6a69bebd722967a4eb6c7c2a616910
Jay <mirhossein.jalali@gmail.com>
2026-01-08 10:08:49 +0800

    Initial Commit

---
+++ FSC Safety Manager/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FSC Safety Manager
+//
+//  Created by Jay on 8/1/2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
