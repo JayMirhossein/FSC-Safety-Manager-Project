07c9d58669048f1dad74a96c1b71346d31056339
Jay <mirhossein.jalali@gmail.com>
2026-01-31 23:44:56 +0800

    FSC-SM Digital Composite Data Point

---
+++ FSC Safety Manager/FSC Config/FS88-504.c
@@ -0,0 +1,8 @@
+//
+//  FS88-504.c
+//  FSC Safety Manager
+//
+//  Created by Jay on 31/1/2026.
+//
+
+#include "FS88-504.h"
---
+++ FSC Safety Manager/FSC Config/FS88-504.h
@@ -0,0 +1,13 @@
+//
+//  FS88-504.h
+//  FSC Safety Manager
+//
+//  Created by Jay on 31/1/2026.
+//
+
+#ifndef FS88_504_h
+#define FS88_504_h
+
+#include <stdio.h>
+
+#endif /* FS88_504_h */
