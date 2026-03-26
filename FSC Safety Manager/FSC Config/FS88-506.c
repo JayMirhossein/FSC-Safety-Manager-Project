9a2f0df1236de9f3fb3753d03b81831753f0f4ed
Jay <mirhossein.jalali@gmail.com>
2026-03-26 23:45:16 +0800

    fixed function

---
+++ FSC Safety Manager/FSC Config/FS88-506.c
@@ -0,0 +1,8 @@
+//
+//  FS88-506.c
+//  FSC Safety Manager
+//
+//  Created by Jay on 26/2/2026.
+//
+
+#include "FS88-506.h"
---
+++ FSC Safety Manager/FSC Config/FS88-506.h
@@ -0,0 +1,13 @@
+//
+//  FS88-506.h
+//  FSC Safety Manager
+//
+//  Created by Jay on 26/2/2026.
+//
+
+#ifndef FS88_506_h
+#define FS88_506_h
+
+#include <stdio.h>
+
+#endif /* FS88_506_h */
