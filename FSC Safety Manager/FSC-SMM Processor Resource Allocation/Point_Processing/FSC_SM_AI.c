4733ea5b6de1c2f8c42dce76cddb3d8313424d77
Jay <mirhossein.jalali@gmail.com>
2026-04-25 23:22:19 +0800

    added readiing AI

---
+++ FSC Safety Manager/FSC-SMM Processor Resource Allocation/Point_Processing/FSC_SM_AI.c
@@ -0,0 +1,8 @@
+//
+//  FSC_SM_AI.c
+//  FSC Safety Manager
+//
+//  Created by Jay on 25/4/2026.
+//
+
+#include "FSC_SM_AI.h"
---
+++ FSC Safety Manager/FSC-SMM Processor Resource Allocation/Point_Processing/FSC_SM_AI.h
@@ -0,0 +1,13 @@
+//
+//  FSC_SM_AI.h
+//  FSC Safety Manager
+//
+//  Created by Jay on 25/4/2026.
+//
+
+#ifndef FSC_SM_AI_h
+#define FSC_SM_AI_h
+
+#include <stdio.h>
+
+#endif /* FSC_SM_AI_h */
