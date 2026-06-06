b137fd77d1d2f94666cd420d302d2d53060f8627
Jayzaiabari <jay.ziabari@gmail.com>
2026-06-06 13:26:53 +0800

    file compare

---
+++ FSC Safety Manager/FSC-SMM Processor Resource Allocation/Data Tables/Data_Table.c
@@ -0,0 +1,8 @@
+//
+//  Data_Table.c
+//  FSC Safety Manager
+//
+//  Created by Jay on 5/6/2026.
+//
+
+#include "Data_Table.h"
---
+++ FSC Safety Manager/FSC-SMM Processor Resource Allocation/Data Tables/Data_Table.h
@@ -0,0 +1,13 @@
+//
+//  Data_Table.h
+//  FSC Safety Manager
+//
+//  Created by Jay on 5/6/2026.
+//
+
+#ifndef Data_Table_h
+#define Data_Table_h
+
+#include <stdio.h>
+
+#endif /* Data_Table_h */
